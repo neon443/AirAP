98c6efe0fc45b8632c50d8d5019b773e3249b284
neon443 <69979447+neon443@users.noreply.github.com>
2025-05-16 11:45:58 +0100

    create xcodeproj

---
+++ AirAP/AirAPApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirAPApp.swift
+//  AirAP
+//
+//  Created by neon443 on 16/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct AirAPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
