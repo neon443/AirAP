23b855dd2a13623c0094908ff8c9d8c848e1ef0d
neon443 <69979447+neon443@users.noreply.github.com>
2025-05-16 11:26:50 +0100

    Initial Commit

---
+++ AirReciever/AirRecieverApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirRecieverApp.swift
+//  AirReciever
+//
+//  Created by neon443 on 16/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct AirRecieverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
