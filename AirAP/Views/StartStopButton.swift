a356551b298cfe6838034944c838839d4c04c28b
neon443 <69979447+neon443@users.noreply.github.com>
2025-05-20 21:28:53 +0100

    added start stop button to settings and a help text

---
+++ AirAP/Views/StartStopButton.swift
@@ -0,0 +1,18 @@
+//
+//  StartStopButton.swift
+//  AirAP
+//
+//  Created by neon443 on 20/05/2025.
+//
+
+import SwiftUI
+
+struct StartStopButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StartStopButton()
+}
