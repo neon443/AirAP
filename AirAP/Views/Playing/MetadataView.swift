cf63fd0ce5f969a3e679333644a9b4589aaeb945
neon443 <69979447+neon443@users.noreply.github.com>
2025-05-21 23:03:28 +0100

    cusotm modifers are really clutch

--- AirAP/Views/Playing/MetadataView.swift
+++ AirAP/Views/Playing/MetadataView.swift
@@ -9,37 +9,52 @@ import SwiftUI
 
 struct MetadataView: View {
 	@ObservedObject var ASmanager: AirstreamManager
-    var body: some View {
-		Text(ASmanager.title ?? "Not Playing")
-			.bold()
-			.font(.title)
-			.contentTransition(.numericText())
-			.multilineTextAlignment(.center)
-			.foregroundStyle(
-				.primary.opacity(ASmanager.title != nil ? 1 : 0.5)
-			)
-			.padding(.bottom)
-		
-		Text("album")
-			.foregroundStyle(.gray)
-			.font(.subheadline)
-		Text(ASmanager.album ?? " ")
-			.font(.title3)
-			.contentTransition(.numericText())
-			.multilineTextAlignment(.center)
-			.frame(maxWidth: .infinity)
-		
-		Text("artist")
-			.foregroundStyle(.gray)
-			.font(.subheadline)
-		Text(ASmanager.artist ?? " ")
-			.font(.title3)
-			.contentTransition(.numericText())
-			.multilineTextAlignment(.center)
-			.frame(maxWidth: .infinity)
-    }
+	var body: some View {
+		VStack(alignment: .leading) {
+			Text(ASmanager.title ?? "Not Playing")
+				.bold()
+				.modifier(NEText())
+				.foregroundStyle(
+					.primary.opacity(ASmanager.title != nil ? 1 : 0.5)
+				)
+				.padding(.bottom)
+			
+			Text("album")
+				.modifier(NEHeading())
+			Text(ASmanager.album ?? " ")
+				.modifier(NEText())
+			
+			Text("artist")
+				.modifier(NEHeading())
+			Text(ASmanager.artist ?? " ")
+				.modifier(NEText())
+			
+			Text("sample rate")
+				.modifier(NEHeading())
+			Text("\(ASmanager.airstream?.sampleRate)")
+				.modifier(NEText())
+		}
+		.padding(.horizontal)
+	}
 }
 
 #Preview {
     MetadataView(ASmanager: AirstreamManager())
+}
+
+fileprivate struct NEHeading: ViewModifier {
+	func body(content: Content) -> some View {
+		content
+			.foregroundStyle(.gray)
+			.font(.subheadline)
+	}
+}
+
+fileprivate struct NEText: ViewModifier {
+	func body(content: Content) -> some View {
+		content
+			.font(.title3)
+			.contentTransition(.numericText())
+			.multilineTextAlignment(.center)
+	}
 }
