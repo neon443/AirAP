d8b96e0a1fd0176e91ae4a7bf2cd8d8661182e52
Nihaal Sharma <69979447+neon443@users.noreply.github.com>
2026-09-06 00:49:11 +0100

    update helpview

--- AirAP/UIKit/HelpViewController.swift
+++ AirAP/UIKit/HelpViewController.swift
@@ -17,37 +17,96 @@ class HelpViewController: UITableViewController {
 		fatalError("init(coder:) has not been implemented")
 	}
 	
+	enum Topic: Int, CaseIterable {
+		case doesNotAppear = 0
+		case runOnOldDevice = 1
+		case iHaveFeedback = 2
+	}
+	
 	override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
 		let cell = UITableViewCell()
 		
 		var config = cell.defaultContentConfiguration()
-		switch indexPath.section {
-		case 0:
-			switch indexPath.row {
-			case 0:
+		
+		let topic = Topic(rawValue: indexPath.section)
+		switch topic {
+		case .doesNotAppear:
+			if indexPath.row == 0 {
 				config.image = UIImage(systemName: "1.circle")
 				config.text = "Open AirPlay picker on other device"
-			case 1:
+			} else {
 				config.image = UIImage(systemName: "2.circle")
 				config.text = "Toggle server on and off"
-			default:
-				print("row invalid")
 			}
-		default:
-			print("section invalid")
+		case .runOnOldDevice:
+			config.text = "Sideload the .ipa file from GitHub"
+		case .iHaveFeedback:
+			config.text = "Submit feedback via TestFlight, or open a GitHub issue"
+		case nil:
+			fatalError("invalid section \(indexPath) to topic")
 		}
-		
 		
 		cell.contentConfiguration = config
 		return cell
 	}
 	
 	override func numberOfSections(in tableView: UITableView) -> Int {
-		return 1
+		return Topic.allCases.count
 	}
 	
 	override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-		return 2
+		let topic = Topic(rawValue: section)
+		switch topic {
+		case .doesNotAppear:
+			return 2
+		case .runOnOldDevice, .iHaveFeedback:
+			return 1
+		case nil:
+			fatalError("invalid section \(section) to topic")
+		}
 	}
 	
+	override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+		let topic = Topic(rawValue: section)
+		switch topic {
+		case .doesNotAppear:
+			return "Does not appear in AirPlay picker"
+		case .runOnOldDevice:
+			return "Run server on old iOS devices"
+		case .iHaveFeedback:
+			return "I have feedback!"
+		case nil:
+			fatalError("invalid section \(section) to topic")
+		}
+	}
+	
+	override func tableView(_ tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
+		let topic = Topic(rawValue: section)
+		switch topic {
+		case .doesNotAppear:
+			return nil
+		case .runOnOldDevice:
+			let button = UIButton(type: .custom)
+			button.addTarget(self, action: #selector(openReleses), for: .touchUpInside)
+			button.setTitle("Open GitHub", for: .normal)
+			button.setTitleColor(.systemBlue, for: .normal)
+			return button
+		case .iHaveFeedback:
+			let button = UIButton(type: .custom)
+			button.addTarget(self, action: #selector(createAnIssue), for: .touchUpInside)
+			button.setTitle("Create a GitHub issue", for: .normal)
+			button.setTitleColor(.systemBlue, for: .normal)
+			return button
+		case nil:
+			fatalError("invalid section \(section) to topic")
+		}
+	}
+	
+	@objc func openReleses() {
+		UIApplication.shared.open(URL(string: "https://github.com/neon443/AirAP/releases/latest")!)
+	}
+	
+	@objc func createAnIssue() {
+		UIApplication.shared.open(URL(string: "https://github.com/neon443/AirAP/issues/new/choose")!)
+	}
 }
---
+++ AirAP/UIKit/SettingsViewController.swift
@@ -0,0 +1,8 @@
+//
+//  SettingsViewController.swift
+//  AirAP
+//
+//  Created by neon443 on 05/09/2026.
+//
+
+import Foundation
